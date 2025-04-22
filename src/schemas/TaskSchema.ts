d74564ece8d2cda4adc3b217c217d4008912e77d
Gabryel <gabryel.macedo@gmail.com>
2025-04-21 23:10:20 -0300

    refactor: update TaskSchema to use enums and default values
    add TypeScript enums (Status, Priority) for status and priority validation.
    
    made status and priority optional with default values (Todo, Low).

--- src/schemas/TaskSchema.ts
+++ src/schemas/TaskSchema.ts
@@ -1,9 +1,34 @@
-import {z} from "zod";
+import { z } from "zod";
+import { Status, Priority } from "../entities/Task";
 
-const createTask = z.object({
-    title: z.string().min(1, {message: "title is required."}).max(100, {message: "title must be at most 100 characters."}),
-    description: z.string().min(1, {message: "description is required."}).max(255, {message: "description at most 255 characters."}),
-    status: z.enum(["Todo", "In-Progress", "Done"], {message: "status must be one of: Todo, In-Progress, Done."}),
-    priority: z.enum(["Low", "Medium", "High"], {message: "priority must be one of: Low, Medium, High."}),
-    category: z.string().max(50, {message: "category must be at most 50 characters. "}).optional(),
+const statusValues = Object.values(Status) as [string, ...string[]];
+const priorityValues = Object.values(Priority) as [string, ...string[]];
+
+export const TaskSchema = z.object({
+	title: z
+		.string()
+		.min(1, { message: "title is required." })
+		.max(100, { message: "title must be at most 100 characters." }),
+	description: z
+		.string()
+		.min(1, { message: "description is required." })
+		.max(255, { message: "description at most 255 characters." }),
+	status: z
+		.enum(statusValues, {
+			message: "status must be one of: Todo, In-Progress, Done",
+		})
+		.optional()
+		.default(Status.Todo),
+	priority: z
+		.enum(priorityValues, {
+			message: "priority must be one of: Low, Medium, High",
+		})
+		.optional()
+		.default(Priority.Low),
+	category: z
+		.string()
+		.max(50, { message: "category must be at most 50 characters." })
+		.optional(),
 });
+
+export type TaskDTO = z.infer<typeof TaskSchema>;
